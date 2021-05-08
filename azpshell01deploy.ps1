ccecd2ed6608fb37a20944447a096ed07ab8b307
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-05-09 07:32:04 +1000

    addTags

--- azpshell01deploy.ps1
+++ azpshell01deploy.ps1
@@ -1,8 +1,9 @@
 # Deploy the template
 $templateFile = "azuredeploy.json"
 $today=Get-Date -Format "dd-MM-yyyy"
-$deploymentName="addVariable-"+"$today"
+$deploymentName="updateTags-"+"$today"
 New-AzResourceGroupDeployment `
   -Name $deploymentName `
   -TemplateFile $templateFile `
-  -storagePrefix mystrg01
+  -storagePrefix mystrg01 `
+  -storageSKU Standard_LRS
