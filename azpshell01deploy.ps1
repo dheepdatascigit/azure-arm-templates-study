5031bf69b32f730ab59641bdf959742359959cb8
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-05-08 22:40:32 +1000

    parameter change

--- azpshell01deploy.ps1
+++ azpshell01deploy.ps1
@@ -1,8 +1,9 @@
 # Deploy the template
 $templateFile = "azuredeploy.json"
 $today=Get-Date -Format "dd-MM-yyyy"
-$deploymentName="addstorage-"+"$today"
+$deploymentName="addnameparameter_change-"+"$today"
 New-AzResourceGroupDeployment `
   -Name $deploymentName `
   -TemplateFile $templateFile `
-  -storageAccountType Standard_LRS
+  -storageAccountType Standard_LRS `
+  -storageName learnexercisenew334634
