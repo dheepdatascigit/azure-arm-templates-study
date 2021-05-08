3d06ab1650c569f2a800177de13a8bfc7df5d605
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-05-09 06:55:38 +1000

    addfunction

--- azpshell01.ps1
+++ azpshell01.ps1
@@ -11,12 +11,5 @@ $context = Get-AzSubscription -SubscriptionId $conciergesub.Id
 Set-AzContext $context
 
 # set default group to the one automatically created in sandbox
-Set-AzDefault -ResourceGroupName learn-8966c842-dd73-4d69-a4a3-66739dbee752
-
-# Deploy the template
-$templateFile = "azuredeploy.json"
-$today=Get-Date -Format "dd-MM-yyyy"
-$deploymentName="addstorage-"+"$today"
-New-AzResourceGroupDeployment `
-  -Name $deploymentName `
-  -TemplateFile $templateFile
+# Set-AzDefault -ResourceGroupName learn-8966c842-dd73-4d69-a4a3-66739dbee752
+Set-AzDefault -ResourceGroupName learn-be51576f-624f-4fb6-9010-6fd70f3906c1
--- azpshell01deploy.ps1
+++ azpshell01deploy.ps1
@@ -1,10 +1,8 @@
 # Deploy the template
 $templateFile = "azuredeploy.json"
 $today=Get-Date -Format "dd-MM-yyyy"
-$deploymentName="addSkuParameterOutputs-"+"$today"
+$deploymentName="addfunction-"+"$today"
 New-AzResourceGroupDeployment `
   -Name $deploymentName `
   -TemplateFile $templateFile `
-  -storageAccountType Standard_LRS `
-  -storageName learnexercisenew334634 `
-  -storageSKU Standard_GRS
+  -storagePrefix mystrg01
