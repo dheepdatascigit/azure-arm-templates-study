145ad161c272728469816473d8e24d61930562a6
Dheep Balaraman <dheep.balaraman@kineticit.com.au>
2021-05-10 06:48:12 +1000

    provide parameter file

--- azpshell04deploy.ps1
+++ azpshell04deploy.ps1
@@ -1,9 +1,11 @@
 $parameters = @{vnetName = "VNet-001"; costCenterIO = "12345"; ownerName = "John Smith"}
 $templateFile=".\JSON\maintemplate.json"
+$TemplateParameterFile= ".\JSON\maintemplate.parameters.json"
 $today=Get-Date -Format "dd-MM-yyyy"
-$DeploymentName="DeployLocalTemplate-"+"$today"
+$DeploymentName="DeployLocalTemplate-3-"+"$today"
 
 New-AzResourceGroupDeployment `
 -Name $DeploymentName `
 -TemplateFile $templateFile `
--TemplateParameterObject $parameters
+-TemplateParameterFile $TemplateParameterFile
+
